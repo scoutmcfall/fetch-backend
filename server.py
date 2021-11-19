5b23d09d5168b0b6331c0bc91baa8b879652a181
Scout McFall <scoutmknz@gmail.com>
2021-11-19 23:26:14 +0000

    fix transaction route

--- server.py
+++ server.py
@@ -7,26 +7,33 @@ import os
 import requests
 from jinja2 import StrictUndefined
 from datetime import date, timedelta, datetime
-
+import json
 
 app = Flask(__name__)
 
 app.secret_key = "dev" 
 app.jinja_env.undefined = StrictUndefined
 
-API_KEY = os.environ["API_KEY"]
-
-@app.route('/')
-def homepage():
-    #should this page have a form to collect transaction inputs?
-    #and a button for each route?
-    return render_template("homepage.html")
+# API_KEY = os.environ["API_KEY"]
 
 
-@app.route('/transaction')
-def add_transaction(payer, points, timestamp):
+@app.route('/transaction', methods=["POST"])
+def add_transaction():
     """Add transactions for a specific payer and date."""
-    
+    #getting a post request (from user or somewhere else)    
+    input_json = request.get_json(force=True) 
+    print(type(input_json))
+    # if input_json is not list:
+    #     input_json = [input_json]
+    for transaction in input_json:
+        payer = transaction["payer"]
+        points = transaction["points"]
+        timestamp = transaction["timestamp"]
+        payer_var = session.get(payer,[])
+        payer_var.append((points, timestamp))
+        session[payer] = payer_var
+    return ("", 200)
+
 
 @app.route('/spend')
 def spend_points(points):
@@ -40,6 +47,9 @@ def spend_points(points):
     { "payer": "MILLER COORS", "points": -4,700 }
     ]"""
 
+    #get input from user
+    #update session
+
 @app.route('/balances')
 def all_balances():
     """Return all payer point balances.
@@ -48,3 +58,24 @@ def all_balances():
     "UNILEVER": 0,
     "MILLER COORS": 5300
     }"""
+
+    #just prints session
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    # app.run(debug=True)
+    app.run(debug=True)
+
+    
+
+    test_data = [{ "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" },
+    { "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" },
+    { "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" },
+    { "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" },
+    { "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" }]
+    
+    res = requests.post('http://localhost:5000/transaction', json=test_data)
+
+    print(res) #to see if request works
+    print(res.data)
