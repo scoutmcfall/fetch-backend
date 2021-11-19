ee1a08bd8fad2d7eafed7aa77166b41110747804
Scout McFall <scoutmknz@gmail.com>
2021-11-19 19:52:37 +0000

    set up server routes

--- server.py
+++ server.py
@@ -19,3 +19,18 @@ API_KEY = os.environ["API_KEY"]
 @app.route('/')
 def homepage():
     res = requests.get(url)
+
+
+
+@app.route('/transaction')
+def add_transaction(payer, date):
+    """Add transactions for a specific payer and date."""
+
+@app.route('/spend')
+def spend_points():
+    """● Spend points using the rules above and return a list of 
+    { "payer": <string>, "points": <integer> } for each call."""
+
+@app.route('/balances')
+def all_balances():
+    """Return all payer point balances."""
